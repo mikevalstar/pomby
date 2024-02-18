784ca6a004d3fd7020d7c1362496cf41505f4ca0
Mike Valstar <mike@valstar.dev>
2024-02-18 09:22:15 -0500

    ui shadcn setup

--- ui/admin/src/App.tsx
+++ ui/admin/src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@/components/ui/button';
+
+import './styles/globals.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='grid grid-cols-10 gap-4'>
+      <div className='col-span-2'>
+        <div data-collapsed={false} className='group py-2 data-[collapsed=true]:py-2'>
+          <nav className='grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2'>
+            <Button
+              variant={'ghost'}
+              size={'sm'}
+              className='justify-start dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white'>
+              Title Here
+            </Button>
+            <Button
+              variant={'ghost'}
+              size={'sm'}
+              className='justify-start dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white'>
+              Title Here
+            </Button>
+            <Button
+              variant={'ghost'}
+              size={'sm'}
+              className='justify-start dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white'>
+              Title Here
+            </Button>
+            <Button
+              variant={'ghost'}
+              size={'sm'}
+              className='justify-start dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white'>
+              Title Here
+            </Button>
+          </nav>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='col-span-8'>
+        <div className='py-2'>
+          <h1 className='text-2xl font-bold'>Title Here</h1>
+          <p className='text-lg'>Content Here</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- ui/admin/vite.config.ts
+++ ui/admin/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    //port: process.env.PORT || 5100,
+    host: 'localhost',
+    open: true,
+    proxy: {
+      '/graphql': 'http://localhost:3000',
+    },
+  },
+});
